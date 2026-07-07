6037ddb2380deb2f388f939c6edd8b420219de36
justinamukelanimathebula-jpg <justinamukelanimathebula-jpg@github.com>
2026-07-07 05:19:11 +0200

    Update firebase config to use Client SDK

---
+++ js/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDbOi2aD5eFsiUi6mfFTS-wOj-jWbi5-9M",
+  authDomain: "local-link-mm.firebaseapp.com",
+  projectId: "local-link-mm",
+  storageBucket: "local-link-mm.firebasestorage.app",
+  messagingSenderId: "945184893851",
+  appId: "1:945184893851:web:c84bf0bc2db2d2c81a4f3e"
+};
